fe9596ceef76e3450760804cc81c1f0df1caecf3
Djalma Henrique Silva Lima <djalma@manifestohut.com.br>
2021-11-10 23:45:52 -0300

    add atribuicao e comando

--- src/AnalisadorSintatico/IbiParser.java
+++ src/AnalisadorSintatico/IbiParser.java
@@ -11,58 +11,134 @@ public class IbiParser {
         this.scanner = scanner;
     }
 
+    // ATRIBUICAO
     public void A() {
         Al();
         B();
         E();
+        P();
         C();
     }
 
+    // EXPRESSAO ARITMETICA
     public void E() {
         T();
         El();
     }
 
+    // EXPRESSAO ARITMETICA LOOP
     public void El() {
         token = scanner.nextToken();
-        if (token != null) {
+        if (token.getType() != Token.TK_SPECIAL) {
             OP();
             T();
             El();
+        } else {
+            scanner.back();
         }
     }
 
+    // =
     public void B() {
         token = scanner.nextToken();
-        if (token.getText().compareTo("=") != 0) {
-            throw new ibiSyntaxException("= expected!, found " + Token.TK_TEXT[token.getType()] + " (" + token.getText() + ") at Line " + token.getLine() + " and column " + token.getColumn());
+        if (token.getType() != Token.TK_ARITHMETIC) {
+            throw new ibiSyntaxException("= expected!, found " + Token.TK_TEXT[token.getType()] + " (" + token.getText()
+                    + ") at Line " + token.getLine() + " and column " + token.getColumn());
         }
     }
 
-    public void C() {
+    // ;
+    public void P() {
         token = scanner.nextToken();
-        if (token.getText().compareTo(";") != 0) {
-            throw new ibiSyntaxException("; expected!, found " + Token.TK_TEXT[token.getType()] + " (" + token.getText() + ") at Line " + token.getLine() + " and column " + token.getColumn());
+        if (token.getType() != Token.TK_SPECIAL) {
+            throw new ibiSyntaxException("; expected!, found " + Token.TK_TEXT[token.getType()] + " (" + token.getText()
+                    + ") at Line " + token.getLine() + " and column " + token.getColumn());
         }
     }
 
+    // ID
     public void Al() {
         token = scanner.nextToken();
         if (token.getType() != Token.TK_IDENTIFIER) {
-            throw new ibiSyntaxException("ID expected!, found " + Token.TK_TEXT[token.getType()] + " (" + token.getText() + ") at Line " + token.getLine() + " and column " + token.getColumn());
+            throw new ibiSyntaxException("ID expected!, found " + Token.TK_TEXT[token.getType()] + " ("
+                    + token.getText() + ") at Line " + token.getLine() + " and column " + token.getColumn());
         }
     }
 
+    // NUMEROS EXPRESSAO ARITMETICA
     public void T() {
         token = scanner.nextToken();
         if (token.getType() != Token.TK_IDENTIFIER && token.getType() != Token.TK_INT) {
-            throw new ibiSyntaxException("ID or NUMBER expected!, found " + Token.TK_TEXT[token.getType()] + " (" + token.getText() + ") at Line " + token.getLine() + " and column " + token.getColumn());
+            throw new ibiSyntaxException("ID or NUMBER expected!, found " + Token.TK_TEXT[token.getType()] + " ("
+                    + token.getText() + ") at Line " + token.getLine() + " and column " + token.getColumn());
         }
     }
 
+    // OPERADOR ARITMETICO
     public void OP() {
         if (token.getType() != Token.TK_ARITHMETIC) {
-            throw new ibiSyntaxException("Operator Expected!, found " + Token.TK_TEXT[token.getType()] + " (" + token.getText() +  ") at Line " + token.getLine() + " and column " + token.getColumn());
+            throw new ibiSyntaxException("Operator Expected!, found " + Token.TK_TEXT[token.getType()] + " ("
+                    + token.getText() + ") at Line " + token.getLine() + " and column " + token.getColumn());
+        }
+    }
+
+    // IF ELSE
+    public void C() {
+        token = scanner.nextToken();
+        if (token.getType() == Token.TK_RESERVED) {
+            PA();
+            ER();
+            PA();
+            scanner.back();
+            C();
+            ES();
+        } else if(token.getType() == Token.TK_IDENTIFIER) {
+            scanner.back();
+            A();
+        }
+    }
+
+    // PALAVRA RESERVADA
+    public void PR() {
+        token = scanner.nextToken();
+        if (token.getType() != Token.TK_RESERVED) {
+            throw new ibiSyntaxException("Reserved Word expected!, found " + Token.TK_TEXT[token.getType()] + " ("
+                    + token.getText() + ") at Line " + token.getLine() + " and column " + token.getColumn());
+        }
+    }
+
+    // ( )
+    public void PA() {
+        token = scanner.nextToken();
+        if (token.getType() != Token.TK_SPECIAL) {
+            throw new ibiSyntaxException("Caracter Special expected!, found " + Token.TK_TEXT[token.getType()] + " ("
+                    + token.getText() + ") at Line " + token.getLine() + " and column " + token.getColumn());
+        }
+    }
+
+    // }
+    public void PE() {
+        token = scanner.nextToken();
+        if (token.getType() != Token.TK_SPECIAL) {
+            throw new ibiSyntaxException("Caracter Special expected!, found " + Token.TK_TEXT[token.getType()] + " ("
+                    + token.getText() + ") at Line " + token.getLine() + " and column " + token.getColumn());
+        }
+    }
+
+    // EXPRESSAO RELACIONAL
+    public void ER() {
+
+    }
+
+    // ELSE
+    public void ES() {
+        token = scanner.nextToken();
+        if (token.getType() == Token.TK_SPECIAL) {
+            PR();
+            C();
+            PE();
+        } else {
+            scanner.back();
         }
     }
 
